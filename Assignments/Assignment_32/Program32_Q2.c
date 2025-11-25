4c89df8ed3eedcadf469f7d79fcbcee5ac38ff35
Vrundarank <vrudrankparite@gmail.com>
2025-11-25 23:41:17 +0530

    Added solutions for Assignment_32 with sample test case

---
+++ Assignments/Assignment_32/Program32_Q2.c
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(j > i)
+            {
+                printf("*\t");
+            }
+            else if(j == i)
+            {
+                printf("#\t");
+            }
+            else        // j < i
+            {
+                printf("@\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of Rows: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of Columns: ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Like Test cases handled by application
+//
+//  Input   :   iRow : 4    iCol : 4 
+//
+//  Output  :   *       *       *       #
+//              *       *       #       @
+//              *       #       @       @
+//              #       @       @       @
+//
+////////////////////////////////////////////////////////////////////////
